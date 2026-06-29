649d1ed29186be1afe9df46be45b4a7f07a715bc
Jesse Sorman <jessesorman@gmail.com>
2026-06-29 16:22:14 -0400

    refactor favorites styling

--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -60,7 +60,6 @@ function Favorites() {
                       {favorite.name || `Market #${favorite.id}`}
                     </a>
                   </span>
-                  <br />
                   <button
                     type="button"
                     className="market-remove-favorite-button"
